ec1c6184ddf7d49f8e4faf6fad8c553453fe7b56
llolo12 <crbonillaro@est.utn.ac.cr>
2024-06-03 20:27:46 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+//Call external library
+//import java.util.Scanner;
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//         //ejerciocio1
+//          int n = 0;
+//          int sumatoria = 0;
+//
+//        System.out.println("Ingrese el valor de n ");
+//        n = scanner.nextInt();
+////
+//
+//
+//
+//          for (int i = 1; i<=n;i++){
+//              int a,b,c = 0;
+//              System.out.println("Ingrese el valor de a");
+//        a = scanner.nextInt();
+//        System.out.println("Ingrese el valor de b");
+//        b = scanner.nextInt();
+//        System.out.println("Ingrese el valor de c");
+//        c = scanner.nextInt();
+//
+//
+//        sumatoria= sumatoria + (a-(b+c));
+//
+//        System.out.println("La sumatoria es "+ sumatoria);
+//          }
+
+//       //ejercicio2
+//        double a, b, c, resultado = 0;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Ingrese el valor de a");
+//        a = scanner.nextDouble();
+//        System.out.println("Ingrese el valor de b");
+//        b = scanner.nextDouble();
+//        System.out.println("Ingrese el valor de c");
+//        c = scanner.nextDouble();
+//
+//        double numerador = b*b + (c * 3);
+//        System.out.println("El valor del numerador (b^2+(c*3)) es: "+ numerador);
+//
+//        double denominador = (a-2) / c;
+//        System.out.println("El valor del denominador ((a-2)/c) es: "+ denominador);
+//
+//        resultado=numerador/denominador;
+//
+//        System.out.println("Resultado de la formula es " +resultado);
+//
+//        scanner.close();
+
+
+
+
+    }
+}
